b163f174674c588c322225aea50c996bff3e7d27
yusupovbekmurod <bekmurodyusupov81@gmail.com>
2023-02-10 08:48:49 +0500

    test2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <header>
+      <nav className='nav'>
+11
+      </nav>
+     </header>
+     <main className='main'>
+      <div className='search'>12</div>
+      <div className='content'>13</div>
+     </main>
     </div>
   );
 }
